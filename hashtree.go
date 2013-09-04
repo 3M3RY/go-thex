ce88cc8c7f3b12484b7c9f63e0a901224130c0e4
Emery Hemingway <emery@fuzzlabs.org>
2013-09-03 21:01:09 -0400

    renamed to hashtree, fixed a little bug in Write

--- hashtree.go
+++ hashtree.go
@@ -1,22 +1,36 @@
-// Package tree implements hash tree computation as defined in the following:
+// Copyright © 2013 Emery Hemingway
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Package hashtree implements hash tree computation as defined in the following:
 // http://web.archive.org/web/20080316033726/http://www.open-content.net/specs/draft-jchapweske-thex-02.html
 // R. C. Merkle, A digital signature based on a conventional encryption function, Crypto '87
 //
-// tree is hash algorithm agnostic, New returns a new hash.Hash interface 
+// hashtree is hash algorithm agnostic, New returns a new hash.Hash interface
 // with a the hash.Hash argument as the internal algorithm.
 //
-// Do not pass the data you wish to build a tree out of to Write.
-// Hash the source data in chunks and then pass those chunks to Write.
-// The recommended chunk size is 1024 bytes.
+// Do not directly pass the data being hashed, ash the source data in chunks
+// and then pass those chunks to Write. The recommended chunk size is 1024 bytes.
 //
 // Do note that this implementation follows the Tree Hash EXchange format
 // recommendation and prepends each leaf hash with a zero byte. This is to
-// decrease the collision rate between external chunk hashes and internal 
+// decrease the collision rate between external chunk hashes and internal
 // leaf hashes. Be aware this policy may be different from other tree specs.
 //
 // Because Write takes hash leaves rather than chunks of data, intermediate
 // leaf levels may be verified.
-package tree
+package hashtree
 
 import (
 	"hash"
@@ -32,66 +46,66 @@ type tree struct {
 	sum      chan []byte
 }
 
-// New returns a new hash.Hash that computes the root of a 
+// New returns a new hash.Hash that computes the root of a
 // row of tree leaves. The digest argument is the hash.Hash
 // that will be used to process the tree.
 func New(digest hash.Hash) hash.Hash {
-	size := digest.Size()
 	t := &tree{
 		digest: digest,
-		size:   size,
+		size:   digest.Size(),
 	}
 	t.Reset()
 	return t
 }
 
-func (t *tree) Size() int {
-	return t.size
-}
+func (t *tree) BlockSize() int { return t.size }
+func (t *tree) Size() int      { return t.size }
 
-func (t *tree) BlockSize() int {
-	return t.size
-}
-
-// Write accepts serialized leaves of a hash tree, where a leaf is
-// one hash. If an incomplete leaf is passed in a single Write, it 
-// will be completed with the next Write.
-func (t *tree) Write(p []byte) (nn int, err error) {
-	nn = len(p)
+// Write accepts serialized leaf stream, where a leaf is one hash.
+// If an incomplete leaf is passed, passthe rest in the next Write call.
+func (t *tree) Write(p []byte) (n int, err error) {
+	n = len(p)
 	var i int
 	var j int
+
+	// fill up overflow from the last Write
 	if len(t.overflow) > 0 {
-		i := t.size - len(t.overflow)
-		t.leaves <- append(t.overflow, p[:i]...)
-		t.overflow = nil
+		i = t.size - len(t.overflow)
+		if i > n {
+			t.overflow = append(t.overflow, p[:]...)
+		} else {
+			t.leaves <- append(t.overflow, p[:i]...)
+			t.overflow = nil
+		}
 	}
+
 	j = i + t.size
-	for j <= nn {
+	for j <= n {
 		t.leaves <- p[i:j]
 		i = j
 		j += t.size
 	}
-	if i < nn {
+	if i < n {
 		t.overflow = p[i:]
 	}
-	return nn, nil
+	return
 }
 
 func (t *tree) Reset() {
-	t.overflow  = make([]byte, 0, t.size - 1)
+	t.overflow = make([]byte, 0, t.size-1)
 	t.leaves = make(chan []byte) // Buffering this channel has mixed results on speed
-	t.sum   = make(chan []byte)
+	t.sum = make(chan []byte)
 	go t.processLevel(t.leaves, t.sum)
 }
 
 func (t *tree) Sum(b []byte) []byte {
 	t.leaves <- nil
-	return <- t.sum
+	return <-t.sum
 }
 
 // If it was possible to for tree to hold multiple instances of a Hash
-// then the multiple levels could be hashed simultaneously, if the 
-// channels were buffered
+// then the multiple levels could be hashed simultaneously, if the
+// channels were buffered (I think). -EH
 func (t *tree) processLevel(ingress chan []byte, final chan []byte) {
 	var left []byte
 	var right []byte
--- hashtree_test.go
+++ hashtree_test.go
@@ -1,16 +1,15 @@
 // The test vectors are from
 // http://web.archive.org/web/20080316033726/http://www.open-content.net/specs/draft-jchapweske-thex-02.html#anchor17
 
-package tree_test
+package hashtree_test
 
 import (
 	"bytes"
-	"encoding/base32"
 	"crypto/sha1"
+	"encoding/base32"
+	"github.com/3M3RY/go-hashtree"
 	"testing"
 )
-
-import "code.google.com/p/go-hashtree/tree"
 
 type treeTest struct {
 	desc string
@@ -25,24 +24,22 @@ func init() {
 	var in []byte
 
 	out = "3I42H3S6NNFQ2MSVX7XZKYAYSCX5QBYJ"
-	in  = make([]byte, 0)
+	in = make([]byte, 0)
 	golden = append(golden, treeTest{"an empty (zero-length) buffer", out, in})
 
-	
 	out = "LOUTZHNQZ74T6UVVEHLUEDSD63W2E6CP"
-	in  = []byte{byte(0)}
+	in = []byte{byte(0)}
 	golden = append(golden, treeTest{"a buffer with a single zero byte", out, in})
 
 	out = "ORWD6TJINRJR4BS6RL3W4CWAQ2EDDRVU"
-	in  = make([]byte, 0, 1024)
+	in = make([]byte, 0, 1024)
 	for i := 0; i < 1024; i++ {
 		in = append(in, byte('A'))
 	}
 	golden = append(golden, treeTest{"a buffer with 1024 'A' characters", out, in})
-	
 
 	out = "UUHHSQPHQXN5X6EMYK6CD7IJ7BHZTE77"
-	in  = make([]byte, 0, 1025)
+	in = make([]byte, 0, 1025)
 	for i := 0; i < 1025; i++ {
 		in = append(in, byte('A'))
 	}
@@ -53,7 +50,7 @@ func TestGolden(t *testing.T) {
 	for i := 0; i < len(golden); i++ {
 		g := golden[i]
 		base := sha1.New()
-		tree := tree.New(sha1.New())
+		tree := hashtree.New(sha1.New())
 		for j := 0; j < 4; j++ {
 			base.Write(g.in)
 			leaf := base.Sum(nil)
@@ -69,16 +66,16 @@ func TestGolden(t *testing.T) {
 	}
 }
 
-func BenchMarkGolder(b *testing.B) {
+func BenchmarkGolden(b *testing.B) {
 	b.StopTimer()
 	base := sha1.New()
-	tree := tree.New(base)
+	t := hashtree.New(base)
 	var buf bytes.Buffer
 	for _, g := range golden {
 		buf.Write([]byte(g.in))
 	}
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		tree.Write(buf.Bytes())
+		t.Write(buf.Bytes())
 	}
 }
